98d02474289847abc4ac2acec5d3f922cace9729
Nathan Rathe <nprathe10@gmail.com>
2019-07-31 20:41:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,4 +26,14 @@ class Song
     created_song
   end
 
+  def artist=(artist)
+    @artist = artist
+    @artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    @genre.songs << self if !@genre.songs.include?(self) == true
+  end
+
 end
