a312380177eeb5f23039bb5e630b65c8846a0af1
Nathan Rathe <nprathe10@gmail.com>
2019-07-31 21:11:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,6 +39,17 @@ class Song
     @genre.songs << self if !@genre.songs.include?(self) == true
   end
 
-
+  def self.new_from_filename(file)
+    file_name = file.chomp(".mp3").split(" - ")
+    song = file_name[1]
+    artist = Artist.find_or_create_by_name(file_name[0])
+    genre = Genre.find_or_create_by_name(file_name[2])
+    new_song = self.new(song, artist, genre)
+  end 
+  
+  def self.create_from_filename(file)
+    new_song = self.new_from_filename(file)
+    new_song.save
+  end 
 
 end
