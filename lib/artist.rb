186b6425771395fad6e985d361cc852d1042d867
Nathan Rathe <nprathe10@gmail.com>
2019-07-31 21:14:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,6 +14,10 @@ class Artist
     @@all
   end
 
+  def songs
+    @songs
+  end
+
   def self.destroy_all
     @@all = []
   end
@@ -26,6 +30,17 @@ class Artist
     created_artist = Artist.new(name)
     created_artist.save
     created_artist
+  end
+
+  def add_song(song)
+    if self.songs.include?(song) == false
+      @songs << song
+      song.artist = self if song.artist == nil
+    end
+  end
+
+  def genres
+    @songs.collect {|song| song.genre}.uniq
   end
 
 end
