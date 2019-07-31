03bb98ea1954b49ef55f1a96e7f93704f1af70da
Nathan Rathe <nprathe10@gmail.com>
2019-07-31 22:53:35 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -10,6 +10,9 @@ class MusicLibraryController
     until input == "exit"
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
 
       input = gets.strip
 
