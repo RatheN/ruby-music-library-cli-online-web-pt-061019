eec9d9b6d22e2f7ebcc170fbee32c79d4e739092
Nathan Rathe <nprathe10@gmail.com>
2019-07-31 20:28:03 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,28 @@
+class Genre
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    created_genre = Genre.new(name)
+    created_genre.save
+    created_genre
+  end
+
+end
